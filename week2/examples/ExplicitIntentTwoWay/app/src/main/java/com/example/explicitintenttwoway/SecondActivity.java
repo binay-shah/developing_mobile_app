b1d63d82cc4a9cc63c2fe7bc1304954b69a1aa60
binay-shah <binay18.shah@gmail.com>
2019-02-23 23:25:10 +0545

    initial commit

---
+++ week2/examples/ExplicitIntentTwoWay/app/src/main/java/com/example/explicitintenttwoway/SecondActivity.java
@@ -0,0 +1,40 @@
+package com.example.explicitintenttwoway;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    private EditText mReply;
+
+    public static final String EXTRA_REPLY =
+            "com.example.explicitintenttwoway.extra.REPLY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+
+    public void returnReply(View view) {
+
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
