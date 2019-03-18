2ac00f71ba8bccc83a9950e819480ca453e2f211
binay-shah <binay18.shah@gmail.com>
2019-03-18 07:01:06 +0545

    initial commit

---
+++ week4/examples/Android_todo_first/app/src/main/java/com/example/android_todo_first/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.android_todo_first;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] mTodos;
+    private int mTodoIndex = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /* call the super class onCreate to complete the creation
+         of activity with state changes */
+        super.onCreate(savedInstanceState);
+
+        // set the user interface layout for this Activity
+        setContentView(R.layout.activity_main);
+
+        // initialize member TextView so we can manipulate it later
+        final TextView TodoTextView;
+        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
+        // read the todo array from res/values/strings.xml
+        Resources res = getResources();
+        mTodos = res.getStringArray(R.array.todos);
+
+        // display the first task from mTodo array in the TodoTextView
+        TodoTextView.setText(mTodos[mTodoIndex]);
+
+        Button buttonNext;
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        // OnClick listener for the  Next button
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mTodoIndex += 1;
+                TodoTextView.setText(mTodos[mTodoIndex]);
+            }
+        });
+    }
+
+
+}
+
